9bf0094ab6d1a19189c138ab9a9af064567dbc24
itachi880 <142436626+itachi880@users.noreply.github.com>
2024-10-05 20:04:15 +0100

    nearly ready

---
+++ index.js
@@ -0,0 +1,59 @@
+/**
+ * @description
+ * you can use as many stors as you want to have each store manage a deferent kinde of data
+ * @example
+ * //data1Store.js
+ * export const data=new Store({counter:0})
+ * //compognent1.js
+ * import {data} from "./path to/data1Store"
+ * export default function CompExp (){
+ *  const [data_,setdata]=useState(data.getState())
+ *  useEffect(()=>{
+ *      const unsubscribe=data.subscribe(setdata)
+ *      return ()=>{
+ *      unsubscribe()
+ *      }
+ *  }
+ * ,[])
+ * return <button onclick={()=>{
+ *  data.setState({counter:data_.counter+1})
+ * }}>increess{data_.counter}</button>
+ * }
+ * //compognent2.js
+ *import {data} from "./path to/data1Store"
+ *export default function CompExp (){
+ * const [data_,setdata]=useState(data.getState())
+ * useEffect(()=>{
+ *     const unsubscribe=data.subscribe(setdata)
+ *     return ()=>{
+ *     unsubscribe()
+ *     }
+ * }
+ *,[])
+ *return <p>counter value{data_.counter}</p>
+ *}
+ */
+class Store {
+  #state = null;
+  #listeners = [];
+  constructor(initialState) {
+    this.#state = initialState;
+    this.navigator = () => {};
+  }
+  getState() {
+    return this.#state;
+  }
+
+  setState(newState) {
+    this.#state = { ...this.#state, ...newState };
+    this.#listeners.forEach((listener) => listener(this.#state));
+  }
+
+  subscribe(listener) {
+    this.#listeners.push(listener);
+    return () => {
+      this.#listeners = this.#listeners.filter((l) => l !== listener);
+    };
+  }
+}
+export default Store;
