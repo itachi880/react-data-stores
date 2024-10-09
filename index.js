5785bfc93c60b6bc3feae823d961158d01bd7515
itachi880 <142436626+itachi880@users.noreply.github.com>
2024-10-09 10:43:00 +0100

    intellessens work

--- index.js
+++ index.js
@@ -1,65 +1,42 @@
 import { useState, useEffect } from "react";
+
 /**
- * @description
- * you can use as many stors as you want to have each store manage a deferent kinde of data
- * @example
- * //data1Store.js
- * export const data=new Store({counter:0})
- * //compognent1.js
- * import {data} from "./path to/data1Store"
- * export default function CompExp (){
- *  const [data_,setdata]=useState(data.getState())
- *  useEffect(()=>{
- *      const unsubscribe=data.subscribe(setdata)
- *      return ()=>{
- *      unsubscribe()
- *      }
- *  }
- * ,[])
- * return <button onclick={()=>{
- *  data.setState({counter:data_.counter+1})
- * }}>increess{data_.counter}</button>
- * }
- * //compognent2.js
- *import {data} from "./path to/data1Store"
- *export default function CompExp (){
- * const [data_,setdata]=useState(data.getState())
- * useEffect(()=>{
- *     const unsubscribe=data.subscribe(setdata)
- *     return ()=>{
- *     unsubscribe()
- *     }
- * }
- *,[])
- *return <p>counter value{data_.counter}</p>
- *}
+ * @template T
+ * A simple store class to hold and manage state.
  */
 class Store {
   #state = null;
   #listeners = [];
-  static navigator = () => {};
+
   /**
-   * @template T
-   */
-  /**
-   *
-   * @param {T} initialState
+   * @param {T} initialState - The initial state of the store.
    */
   constructor(initialState) {
-    /**
-     * @type {typeof initialState}
-     */
     this.#state = initialState;
   }
+
+  /**
+   * Returns the current state of the store.
+   * @returns {T} The current state.
+   */
   getState = () => {
     return this.#state;
   };
 
+  /**
+   * Updates the state and notifies all listeners.
+   * @param {Partial<T>} newState - The new state to be merged with the current state.
+   */
   setState = (newState) => {
     this.#state = { ...this.#state, ...newState };
     this.#listeners.forEach((listener) => listener(this.#state));
   };
 
+  /**
+   * Subscribes to state changes.
+   * @param {(state: T) => void} listener - The listener function to be called when the state changes.
+   * @returns {() => void} A function to unsubscribe the listener.
+   */
   subscribe = (listener) => {
     this.#listeners.push(listener);
     return () => {
@@ -67,19 +44,40 @@ class Store {
     };
   };
 }
+
+/**
+ * @template T
+ * A custom hook that allows you to use the store's state in a React component.
+ *
+ * @param {Store<T>} store - The store instance.
+ * @returns {[T, (newState: Partial<T>) => void]} An array containing the current state and the setState function.
+ */
+function useStore(store) {
+  const [state, setState] = useState(store.getState());
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(setState);
+    return () => {
+      unsubscribe();
+    };
+  }, [store]);
+
+  return [state, store.setState];
+}
+
+/**
+ * Creates a store with the given initial state and returns a hook to use the store.
+ *
+ * @template T
+ * @param {T} initialState - The initial state of the store.
+ * @returns {{ useStore: () => [T, (newState: Partial<T>) => void] }} An object containing the `useStore` hook.
+ */
 function createStore(initialState) {
   const store = new Store(initialState);
+
   return {
-    useStore: function () {
-      const [data, setdata] = useState(store.getState());
-      useEffect(() => {
-        const unsubscribe = store.subscribe(setdata);
-        return () => {
-          unsubscribe();
-        };
-      }, []);
-      return [data, store.setState];
-    },
+    useStore: () => useStore(store),
   };
 }
+
 export { Store, createStore };
